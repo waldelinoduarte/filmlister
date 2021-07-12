b62bea81430a7ad9f0729aa557bec5f8557107be
Waldelino Duarte <waldelino@gmail.com>
2021-07-12 17:09:44 -0300

    :tada: Challenge finalized

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
+import { Button } from "./Button";
+
+import { useMovieLister } from "../contexts/MovieContext";
+
+import '../styles/sidebar.scss';
+
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, selectedGenreId, selectGenre } = useMovieLister();
+
+
+  function handleClickButton(id: number) {
+    selectGenre(id);
+  }
+
+  return(
+
+    <nav className="sidebar">
+      
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+
+  )
 }
